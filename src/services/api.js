701a9d7885b70dc2393c76e0033d7d44f9cfa02c
abdoohasgit <abderzakekabbab96@gmail.com>
2025-06-02 19:45:09 +0100

    main

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "484fd0408fc310a2eed190fe9fdd4461";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
